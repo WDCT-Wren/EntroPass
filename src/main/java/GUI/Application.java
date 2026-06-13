f11b72e879a55a3c7c3eb141ed9dd97b50c174fd
WDCT-Wren <wrenctulio@gmail.com>
2026-06-14 00:50:24 +0800

    feat: Updated UI to include logo as well as the official application Icon with initial css for about page

--- src/main/java/GUI/Application.java
+++ src/main/java/GUI/Application.java
@@ -5,23 +5,24 @@ import Database.MasterDAO;
 import GUI.Utils.SceneUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/Assets/Images/EntroPass_Logo.png")));
+
         stage.setWidth(1316);
         stage.setHeight(900);
         stage.setResizable(false);
-
-        //Load fonts in the application
-        Font.loadFont(getClass().getResourceAsStream("org/Assets/Fonts/SpaceGrotesk-Regular.ttf"), 16);
-        Font.loadFont(getClass().getResourceAsStream("org/Assets/Fonts/SpaceGrotesk-Bold.ttf"), 16);
+        stage.getIcons().add(icon);
 
         try {
             DatabaseManager.getInstance().initDatabase();
